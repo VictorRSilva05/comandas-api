fb576f6fa18a98376f357f6d147b1c19a0b3722e
Victor R. Silva <victorr.silva005@gmail.com>
2025-02-25 14:19:19 -0300

    Operações CRUD em Cliente e Produto realizadas

--- src/app/ClienteDAO.py
+++ src/app/ClienteDAO.py
@@ -7,6 +7,115 @@ from infra.orm.ClienteModel import ClienteDB
 
 router = APIRouter()
 
+@router.get("/cliente/", tags=["Cliente"])
+async def get_client():
+    try:
+        session = db.Session()
+        
+        #busca todos
+        dados = session.query(ClienteDB).all()
+        
+        return dados, 200
+    
+    except Exception as e:
+        return{"erro": str(e)}, 400
+    finally:
+        session.close()
+        
+@router.get("/cliente/{id}", tags=["Cliente"])
+async def get_cliente(id: int):
+    try:
+        session = db.Session()
+
+        # busca um com filtro
+        dados = session.query(ClienteDB).filter(ClienteDB.id_cliente == id).all()
+
+        return dados, 200
+
+    except Exception as e:
+        return {"erro": str(e)}, 400
+    finally:
+        session.close()
+        
+@router.post("/cliente/", tags=["Cliente"])
+async def post_cliente(corpo: Cliente):
+    try:
+        session = db.Session()
+    
+        # cria um novo objeto com os dados da requisição
+        dados = ClienteDB(None, corpo.nome, corpo.cpf, corpo.telefone)
+
+        session.add(dados)
+        # session.flush()
+        session.commit()
+        
+        return {"id": dados.id_cliente}, 200
+    
+    except Exception as e:
+        session.rollback()
+        return {"erro": str(e)}, 400
+    finally:
+        session.close()
+
+@router.put("/cliente/{id}", tags=["Cliente"])
+async def put_cliente(id: int, corpo: Cliente):
+    try:
+        session = db.Session()
+        # busca os dados atuais pelo id
+        dados = session.query(ClienteDB).filter(ClienteDB.id_cliente == id).one()
+        # atualiza os dados com base no corpo da requisição
+        dados.nome = corpo.nome
+        dados.cpf = corpo.cpf
+        dados.telefone = corpo.telefone
+        
+        session.add(dados)
+        session.commit()
+        
+        return {"id": dados.id_cliente}, 200
+        
+    except Exception as e:
+        session.rollback()
+        return {"erro": str(e)}, 400
+    finally:
+        session.close()
+        
+@router.put("/cliente/{id}", tags=["Cliente"])
+async def put_cliente(id: int, corpo: Cliente):
+    try:
+        session = db.Session()
+        # busca os dados atuais pelo id
+        dados = session.query(ClienteDB).filter(ClienteDB.id_cliente == id).one()
+        # atualiza os dados com base no corpo da requisição
+        dados.nome = corpo.nome
+        dados.cpf = corpo.cpf
+        dados.telefone = corpo.telefone
+        session.add(dados)
+        session.commit()
+        
+        return {"id": dados.id_cliente}, 200
+        
+    except Exception as e:
+        session.rollback()
+        return {"erro": str(e)}, 400
+    finally:
+        session.close()
+        
+# verifica se o CPF informado já esta cadastrado, retornado os dados atuais caso já esteja
+@router.get("/cliente/cpf/{cpf}", tags=["Cliente - Valida CPF"])
+async def cpf_cliente(cpf: str):
+    try:
+        session = db.Session()
+        
+        # busca um com filtro, retornando os dados cadastrados
+        dados = session.query(ClienteDB).filter(ClienteDB.cpf == cpf).all()
+        
+        return dados, 200
+    
+    except Exception as e:
+        return {"erro": str(e)}, 400
+    finally:
+        session.close()
+        
 # Criar as rotas/endpoints: GET, POST, PUT, DELETE
 
 @router.get("/cliente/", tags=["Cliente"])
--- src/app/ProdutoDAO.py
+++ src/app/ProdutoDAO.py
@@ -9,6 +9,77 @@ router = APIRouter()
 
 @router.get("/produto/", tags=["Produto"])
 async def get_produto():
+    try:
+        session = db.Session()
+        
+        # busca todos
+        dados = session.query(ProdutoDB).all()
+        
+        return dados, 200
+    
+    except Exception as e:
+        return {"erro": str(e)}, 400
+    finally:
+        session.close()
+        
+@router.get("/produto/{id}", tags=["Produto"])
+async def get_produto(id: int):
+    try:
+        session = db.Session()
+
+        # busca um com filtro
+        dados = session.query(ProdutoDB).filter(ProdutoDB.id_produto == id).all()
+
+        return dados, 200
+
+    except Exception as e:
+        return {"erro": str(e)}, 400
+    finally:
+        session.close()
+
+@router.post("/produto/", tags=["Produto"])
+async def post_produto(corpo: Produto):
+    try:
+        session = db.Session()
+    
+        # cria um novo objeto com os dados da requisição
+        dados = ProdutoDB(None, corpo.nome, corpo.tipo, corpo.valor)
+
+        session.add(dados)
+        # session.flush()
+        session.commit()
+        
+        return {"id": dados.id_produto}, 200
+    
+    except Exception as e:
+        session.rollback()
+        return {"erro": str(e)}, 400
+    finally:
+        session.close()
+        
+@router.put("/produto/{id}", tags=["Produto"])
+async def put_produto(id: int, corpo: Produto):
+    try:
+        session = db.Session()
+        # busca os dados atuais pelo id
+        dados = session.query(ProdutoDB).filter(ProdutoDB.id_produto == id).one()
+        # atualiza os dados com base no corpo da requisição
+        dados.nome = corpo.nome
+        dados.tipo = corpo.tipo
+        dados.valor = corpo.valor
+        session.add(dados)
+        session.commit()
+        
+        return {"id": dados.id_funcionario}, 200
+        
+    except Exception as e:
+        session.rollback()
+        return {"erro": str(e)}, 400
+    finally:
+        session.close()
+
+@router.get("/produto/", tags=["Produto"])
+async def get_produto():
     return {"msg": "get todos executado"}, 200
 
 @router.get("/produto/{id}", tags=["Produto"])
